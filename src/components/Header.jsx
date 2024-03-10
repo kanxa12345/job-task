6c5f15604312969b8b0a43211690b4654c4c81b4
Sandip <c.restsandip123@gmail.com>
2024-03-10 14:27:28 +0545

    header & footer responsive done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [nav, setNav] = useState(false);
   return (
     <header>
       <div className="container navbar">
         <a href="#">
           <img src="/images/logo.PNG" alt="logo" className="header-logo" />
         </a>
-        <nav>
+        <button className="hamburger-button" onClick={() => setNav(!nav)}>
+          <i class={`fa-solid fa-bars ${nav ? "hide-icon" : ""}`}></i>
+          <i class={`fa-solid fa-xmark  ${nav ? "" : "hide-icon"}`}></i>
+        </button>
+        <nav className={`${nav ? "show-nav" : ""}`}>
           <ul className="nav">
             <li>
               <a href="#">Home</a>
